974c70cca8e82dbccccbba665a7e1d6d401d650b
LeeMineo <lms4882@gmail.com>
2024-07-22 18:14:48 +0900

    css 문제 해결?

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3c8bd3cfd8ff4e47"; }
+/******/ 		__webpack_require__.h = function() { return "a173fab350f80278"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/app/_app.js
@@ -0,0 +1,8 @@
+// src/app/_app.js
+import '../globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
